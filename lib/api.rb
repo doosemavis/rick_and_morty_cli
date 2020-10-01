5408323da30e27985948beb95309e26210ba7211
doosemavis <moosedavis2011@gmail.com>
2020-10-01 13:04:49 -0500

    rick_and_morty_cli

--- lib/api.rb
+++ lib/api.rb
@@ -7,11 +7,9 @@ class API
         uri=URI(url)
         response=Net::HTTP.get(uri)
         character_info = JSON.parse(response)["results"]
-        
+
         character_info.each do |c|
             character = Character.new(name: c["name"], species: c["species"], gender: c["gender"], status: c["status"]) if c["name"] != nil
         end 
-        # binding.pry 
     end 
-
 end 
